62d33f5ed8177f5c9ba9c6095bd290ce379738ae
tolga <dev.tolgataner@icloud.com>
2022-02-02 19:11:55 +0300

    Add unit test of OtpManager

---
+++ Example/Tests/OtpManagerTests.swift
@@ -0,0 +1,57 @@
+import XCTest
+import SecureKit
+
+final class OtpManagerTests: XCTestCase {
+    
+    private var sut: OtpManager!
+    
+    private enum DummyError: Swift.Error {
+        case cantInitialization
+        case encryptionError
+        case decryptionError
+        case cantStore
+    }
+    
+    override func setUp() {
+        super.setUp()
+        setup()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+    }
+    
+    func testValuesSettingIsCorrect() {
+        XCTAssertTrue(sut.key == "2tC2H19lkVbQDfakxcrtNMQdd0FloLyw")
+        XCTAssertTrue(sut.iv == "bbC2H19lkVbQDfak")
+        XCTAssertTrue(sut.code == "987654")
+    }
+    
+    func testCodeEncryptedSuccessfully() {
+        XCTAssertTrue(sut.code == "987654")
+        let result = try! sut.encrypt()
+        XCTAssertFalse(result == sut.code)
+    }
+    
+    func testCodeDecryptedSuccessfully() {
+        XCTAssertTrue(sut.code == "987654")
+        let encryptedResult = try! sut.encrypt()
+        XCTAssertFalse(encryptedResult == "987654")
+        let result = try! sut.decrypt()
+        XCTAssertTrue(result == "987654")
+    }
+    
+    private func setup() {
+        do {
+            let key = "2tC2H19lkVbQDfakxcrtNMQdd0FloLyw"
+            let iv = "bbC2H19lkVbQDfak"
+            sut = try OtpManager(code: "987654", iv: iv, key: key)
+        }
+        catch {
+            XCTAssertThrowsError(DummyError.cantInitialization)
+        }
+    }
+    
+    
+}
--- SecureKit.podspec
+++ SecureKit.podspec
@@ -35,7 +35,7 @@ Pod::Spec.new do |s|
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+   s.frameworks = 'XCTest'
    s.dependency 'CryptoSwift'
    s.dependency 'KeychainSwift'
 end
